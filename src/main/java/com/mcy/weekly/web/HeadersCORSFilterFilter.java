50bcbfbe54c428ad779c41f2c05df5ca35987913
mcy <806938139@qq.com>
2019-02-13 13:40:20 +0800

    改善

---
+++ src/main/java/com/mcy/weekly/web/HeadersCORSFilterFilter.java
@@ -0,0 +1,28 @@
+package com.mcy.weekly.web;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "HeadersCORSFilterFilter",urlPatterns = "/*")
+public class HeadersCORSFilterFilter implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, If-Modified-Since");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.addHeader("Access-Control-Allow-Credentials", "true");
+        chain.doFilter(req, response);
+    }
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
